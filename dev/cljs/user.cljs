86e8d4b8f67787b653d1d352b9c752eb2b53d662
Daniel Le <greenrecyclebin@gmail.com>
2017-06-26 17:06:00 +0800

    lein new chestnut tictactoe +no-poll +reagent

---
+++ dev/cljs/user.cljs
@@ -0,0 +1,8 @@
+(ns cljs.user
+  (:require [tictactoe.core]
+            [tictactoe.system :as system]))
+
+(def go system/go)
+(def reset system/reset)
+(def stop system/stop)
+(def start system/start)
---
+++ src/clj/tictactoe/application.clj
@@ -0,0 +1,25 @@
+(ns tictactoe.application
+  (:gen-class)
+  (:require [com.stuartsierra.component :as component]
+            [system.components.endpoint :refer [new-endpoint]]
+            [system.components.handler :refer [new-handler]]
+            [system.components.middleware :refer [new-middleware]]
+            [system.components.jetty :refer [new-web-server]]
+            [tictactoe.config :refer [config]]
+            [tictactoe.routes :refer [home-routes]]))
+
+(defn app-system [config]
+  (component/system-map
+   :routes     (new-endpoint home-routes)
+   :middleware (new-middleware {:middleware (:middleware config)})
+   :handler    (-> (new-handler)
+                   (component/using [:routes :middleware]))
+   :http       (-> (new-web-server (:http-port config))
+                   (component/using [:handler]))))
+
+(defn -main [& _]
+  (let [config (config)]
+    (-> config
+        app-system
+        component/start)
+    (println "Started tictactoe on" (str "http://localhost:" (:http-port config)))))
