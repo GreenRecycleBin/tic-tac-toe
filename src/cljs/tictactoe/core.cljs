20f102cabda43f4a488232dc1eb68de4b2003cdb
Daniel Le <greenrecyclebin@gmail.com>
2017-07-06 19:54:43 +0800

    Allow players to take turn to fill the board

--- src/cljs/tictactoe/core.cljs
+++ src/cljs/tictactoe/core.cljs
@@ -5,26 +5,49 @@
 
 (defonce board-size 9)
 (defonce row-count 3)
-(defonce app-state (atom {:player "X"}))
+(defonce app-state (atom {:player "X"
+                          :squares (vec (repeat board-size nil))}))
 
 (defn info [app-state]
   [:div {:class "game-info"}
    [:div "Next player: " (:player @app-state)]])
 
-(defn square [i]
-  [:button.square {:key (str "square-" i)}])
+(defn square [app-state i]
+  (letfn [(next-player [player]
+            (case player
+              "X" "O"
+              "O" "X"))
 
-(defn row [i xs]
-  [:div.board-row {:key (str "row-" i)} (doall (map #(square %) xs))])
+          (toggle-player [state player]
+            (assoc state :player (next-player player)))
 
-(defn board []
+          (record-move [state player i]
+            (assoc-in state [:squares i] player))
+
+          (square-on-click []
+            #(swap! app-state (fn [state]
+                                (let [{:keys [player squares]} state]
+                                  (if-not (get squares i)
+                                    (-> state
+                                        (record-move player i)
+                                        (toggle-player player))
+                                    state)))))]
+    [:button.square
+     {:key (str "square-" i) :on-click (square-on-click)}
+     (get-in @app-state [:squares i])]))
+
+(defn row [app-state i xs]
+  (let [square (partial square app-state)]
+    [:div.board-row {:key (str "row-" i)} (doall (map #(square %) xs))]))
+
+(defn board [app-state]
   [:div {:class "game-board"}
-   (doall (map-indexed #(row %1 %2)
+   (doall (map-indexed #(row app-state %1 %2)
                        (partition row-count (range board-size))))])
 
 (defn game [app-state]
   [:div {:class "game"}
-   (board)
+   (board app-state)
    (info app-state)])
 
 (defn render []
